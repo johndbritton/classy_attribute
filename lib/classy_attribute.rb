067997b5258e092ea4e0a4d0a95a67dc840fcbd7
John Britton <public@johndbritton.com>
2015-01-14 19:10:13 +1100

    spiking out an implementation

--- lib/classy_attribute.rb
+++ lib/classy_attribute.rb
@@ -1,5 +1,28 @@
 require 'classy_attribute/version'
 
 module ClassyAttribute
-  # Your code goes here...
+
+  attr_accessor :value
+
+  def initialize(value)
+    self.value = value
+  end
+
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+
+    def dump(object)
+      return unless object.is_a?(self)
+      object.value
+    end
+
+    def load(value)
+      return if value.nil?
+
+      self.new(value)
+    end
+  end
 end
